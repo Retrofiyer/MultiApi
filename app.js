e8198c4456270d1a38a6253be58f95586de0a2cf
Sebastian <sebitas5225mail.com>
2024-06-03 11:58:34 -0500

    Update Vercel

--- app.js
+++ app.js
@@ -3,17 +3,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Middleware para servir los archivos HTML de los distintos proyectos
 app.use('/marvel', express.static(path.join(__dirname, 'Marvel')));
 app.use('/starwars', express.static(path.join(__dirname, 'StarWars')));
 app.use('/pokemon', express.static(path.join(__dirname, 'Pokemon')));
 
-// Servir la página de inicio con la navbar
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, './Marvel/index.html'));
 });
 
-// Iniciar el servidor
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor escuchando en ${port}...`);
 });
